a8901e4f001cbcbabef7c630790bec26d73493a1
harish-p-exalca <harish.p@exalca.com>
2021-09-29 09:46:06 +0530

    Lease changes 28-sep

--- src/app/service/lease-management.service.ts
+++ src/app/service/lease-management.service.ts
@@ -1,8 +1,9 @@
-import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable, throwError } from 'rxjs';
 import { catchError } from 'rxjs/operators';
 import { AuthService } from 'src/app/service/auth.service';
+import { LeaseDocument, LeaseManagement } from '../Model/Leasemanagement';
 
 @Injectable({
   providedIn: 'root'
@@ -10,8 +11,8 @@ import { AuthService } from 'src/app/service/auth.service';
 export class LeaseManagementService {
 
   baseAddress;
-  constructor(private http: HttpClient,public authService:AuthService) { 
-    this.baseAddress=this.authService.baseAddress;
+  constructor(private http: HttpClient, public authService: AuthService) {
+    this.baseAddress = this.authService.baseAddress;
   }
   // Error Handler
 
@@ -24,12 +25,12 @@ export class LeaseManagementService {
     );
   }
 
-  UploadLeaseDraft(selectedFiles: File[],client="001",company="Exa",site="site1"): Observable<any> {
+  UploadLeaseDraft(selectedFiles: File[], client = "001", company = "Exa", site = "site1"): Observable<any> {
     const formData: FormData = new FormData();
     if (selectedFiles && selectedFiles.length) {
-        selectedFiles.forEach(x => {
-            formData.append(x.name, x, x.name);
-        });
+      selectedFiles.forEach(x => {
+        formData.append(x.name, x, x.name);
+      });
     }
     formData.append('Client', client);
     formData.append('Company', company);
@@ -38,12 +39,42 @@ export class LeaseManagementService {
       formData
     ).pipe(catchError(this.errorHandler));
   }
-  GetLeaseDrafts():Observable<any>{
+  GetLeaseDrafts(): Observable<any> {
     return this.http.get(`${this.baseAddress}api/Lease/GetLeaseDrafts`)
+      .pipe(catchError(this.errorHandler));
+  }
+  GetFileFromLink(link: string) {
+    return this.http.get(`${link}`, { responseType: 'blob' })
+      .pipe(catchError(this.errorHandler));
+  }
+  // AddSignedFileDetail
+  AddSignedFileDetail(signeddetail: LeaseManagement): Observable<any> {
+    return this.http.post<any>(this.baseAddress + 'api/Lease/AddSignedFileDetail', signeddetail,
+      {
+        headers: new HttpHeaders({
+          'Content-Type': 'application/json'
+        })
+      })
+      .pipe(catchError(this.errorHandler));
+  }
+  //AddSignedFileDetail
+
+  AddSignedFile(signeddetail: LeaseDocument, selectedFiles: File): Observable<any> {
+    const formData: FormData = new FormData();
+    formData.append(selectedFiles.name, selectedFiles, selectedFiles.name);
+    formData.append('Client', signeddetail.Client);
+    formData.append('Site', signeddetail.Site);
+    formData.append('Company', signeddetail.Company);
+    return this.http.post<any>(this.baseAddress + 'api/Lease/AddSignedFile',
+      formData,
+    ).pipe(catchError(this.errorHandler));
+  }
+  GetAllSignedLeases(){
+    return this.http.get(`${this.baseAddress}api/Lease/GetAllSignedLeases`)
     .pipe(catchError(this.errorHandler));
   }
-  GetFileFromLink(link:string){
-    return this.http.get(`${link}`,{responseType:'blob'})
+  GetExpiryLeases(){
+    return this.http.get(`${this.baseAddress}api/Lease/GetExpiryLeases`)
     .pipe(catchError(this.errorHandler));
   }
 }
--- src/app/space/space.service.ts
+++ src/app/space/space.service.ts
@@ -2,16 +2,17 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders, HttpErrorResponse } from '@angular/common/http';
 import { Observable, throwError, Subject } from 'rxjs';
 import { catchError } from 'rxjs/operators';
-import { AreaLink, LocLink,AddrLink, DocumentLink, ARA_Space, PartnerLink, DateLink, ContractLink, SiteLink, ARA_Asset } from './spacemodel';
+import { AreaLink, LocLink, AddrLink, DocumentLink, ARA_Space, PartnerLink, DateLink, ContractLink, SiteLink, ARA_Asset } from './spacemodel';
 import { LeaseDocument, LeaseManagement } from '../Model/Leasemanagement';
+import { AuthService } from '../service/auth.service';
 @Injectable({
     providedIn: 'root'
 })
 export class SpaceService {
     constructor(
-        private _httpClient: HttpClient) { }
-   private baseUrlVsign1 = "http://192.168.0.25:8002/"
-//  private baseUrlVsign1 = "http://192.168.0.25:8002/"
+        private _httpClient: HttpClient, private authService: AuthService) { }
+    private baseUrlVsign1 = this.authService.baseAddress;
+    //  private baseUrlVsign1 = "http://192.168.0.25:8002/"
     errorHandler(error: HttpErrorResponse): Observable<string> {
         return throwError(error.error instanceof Object ? error.error.Message ? error.error.Message : error.error : error.error || error.message || 'Server Error');
     }
@@ -36,119 +37,94 @@ export class SpaceService {
     }
     DocumentLocLink(DocLink: DocumentLink) {
         return this._httpClient.post<any>(this.baseUrlVsign1 + "api/Space/PostDocLink", DocLink
-        ) 
+        )
     }
-    ARA_Space(Space: ARA_Space){
+    ARA_Space(Space: ARA_Space) {
         return this._httpClient.post<any>(this.baseUrlVsign1 + "api/Space/PostGeneralSpace", Space
-        ) 
+        )
     }
-    PartnerLink(PartnerLink: PartnerLink){
+    PartnerLink(PartnerLink: PartnerLink) {
         return this._httpClient.post<any>(this.baseUrlVsign1 + "api/Space/PostPartnerLink", PartnerLink
         )
     }
-    DateLink(DateLink: DateLink){
+    DateLink(DateLink: DateLink) {
         return this._httpClient.post<any>(this.baseUrlVsign1 + "api/Space/PostDateLink", DateLink
         )
     }
-    ContractLink(ContractLink: ContractLink){
+    ContractLink(ContractLink: ContractLink) {
         return this._httpClient.post<any>(this.baseUrlVsign1 + "api/Space/PostContractLink", ContractLink
         )
     }
-    GetParterLink():Observable<any | string> {
+    GetParterLink(): Observable<any | string> {
         return this._httpClient.get<any>(this.baseUrlVsign1 + "api/Space/GetPartnerLink")
-        .pipe(catchError(this.errorHandler));
+            .pipe(catchError(this.errorHandler));
     }
-    GetDateLink():Observable<any | string> {
+    GetDateLink(): Observable<any | string> {
         return this._httpClient.get<any>(this.baseUrlVsign1 + "api/Space/GetDateLink")
-        .pipe(catchError(this.errorHandler));
+            .pipe(catchError(this.errorHandler));
     }
-    GetDocLink():Observable<any | string> {
+    GetDocLink(): Observable<any | string> {
         return this._httpClient.get<any>(this.baseUrlVsign1 + "api/Space/GetDocLink")
-        .pipe(catchError(this.errorHandler));
+            .pipe(catchError(this.errorHandler));
     }
-    GetContractLink():Observable<any | string> {
+    GetContractLink(): Observable<any | string> {
         return this._httpClient.get<any>(this.baseUrlVsign1 + "api/Space/GetContractLink")
-        .pipe(catchError(this.errorHandler));
+            .pipe(catchError(this.errorHandler));
     }
-    GetARACompany():Observable<any | string> {
+    GetARACompany(): Observable<any | string> {
         return this._httpClient.get<any>(this.baseUrlVsign1 + "api/Space/GetARACompany")
-        .pipe(catchError(this.errorHandler));
+            .pipe(catchError(this.errorHandler));
     }
-    GetARASite():Observable<any | string> {
+    GetARASite(): Observable<any | string> {
         return this._httpClient.get<any>(this.baseUrlVsign1 + "api/Space/GetARASite")
-        .pipe(catchError(this.errorHandler));
+            .pipe(catchError(this.errorHandler));
     }
-    GetARASpace():Observable<any | string> {
+    GetARASpace(): Observable<any | string> {
         return this._httpClient.get<any>(this.baseUrlVsign1 + "api/Space/GetARASpace")
-        .pipe(catchError(this.errorHandler));
+            .pipe(catchError(this.errorHandler));
     }
-    GetSpaceDetail(Spacename:string):Observable<any | string>{
+    GetSpaceDetail(Spacename: string): Observable<any | string> {
         return this._httpClient.get<any>(`${this.baseUrlVsign1}api/Space/GetSpace?Spacename=${Spacename}`)
-        .pipe(catchError(this.errorHandler));
+            .pipe(catchError(this.errorHandler));
     }
     ARA_SiteLink(site: SiteLink) {
         return this._httpClient.post<any>(this.baseUrlVsign1 + "api/Space/PostSitedetail", site
         )
     }
-    ARA_Asset(Asset: ARA_Asset){
+    ARA_Asset(Asset: ARA_Asset) {
         return this._httpClient.post<any>(this.baseUrlVsign1 + "api/Space/PostGeneralAsset", Asset
-        ) 
+        )
     }
-    Getsitehierarchy():Observable<any> {
+    Getsitehierarchy(): Observable<any> {
         return this._httpClient.get<any>(this.baseUrlVsign1 + "api/Space/GetTreedata")
-        .pipe(catchError(this.errorHandler));
+            .pipe(catchError(this.errorHandler));
     }
-    GetElectricalDistribution():Observable<any> {
+    GetElectricalDistribution(): Observable<any> {
         return this._httpClient.get<any>(this.baseUrlVsign1 + "api/Space/GetElectricalDistributionDetails")
-        .pipe(catchError(this.errorHandler));
+            .pipe(catchError(this.errorHandler));
     }
-    GetEnergyConsumption():Observable<any> {
+    GetEnergyConsumption(): Observable<any> {
         return this._httpClient.get<any>(this.baseUrlVsign1 + "api/Space/GetEnergyConsumptionDetails")
-        .pipe(catchError(this.errorHandler));
+            .pipe(catchError(this.errorHandler));
     }
-    GetWaterConsumption():Observable<any> {
+    GetWaterConsumption(): Observable<any> {
         return this._httpClient.get<any>(this.baseUrlVsign1 + "api/Space/GetWaterConsumptionDetails")
-        .pipe(catchError(this.errorHandler));
+            .pipe(catchError(this.errorHandler));
     }
-    GetPowerResources():Observable<any> {
+    GetPowerResources(): Observable<any> {
         return this._httpClient.get<any>(this.baseUrlVsign1 + "api/Space/GetPowerConsumptionDetails")
-        .pipe(catchError(this.errorHandler));
+            .pipe(catchError(this.errorHandler));
     }
-    GetParkingAccessControl():Observable<any> {
+    GetParkingAccessControl(): Observable<any> {
         return this._httpClient.get<any>(this.baseUrlVsign1 + "api/Space/GetParkingAccessControlDetails")
-        .pipe(catchError(this.errorHandler));
+            .pipe(catchError(this.errorHandler));
     }
-    GetFireAlaram():Observable<any> {
+    GetFireAlaram(): Observable<any> {
         return this._httpClient.get<any>(this.baseUrlVsign1 + "api/Space/GetFireAlaramDetails")
-        .pipe(catchError(this.errorHandler));
+            .pipe(catchError(this.errorHandler));
     }
-    GetOthers():Observable<any> {
+    GetOthers(): Observable<any> {
         return this._httpClient.get<any>(this.baseUrlVsign1 + "api/Space/GetOtherDetails")
-        .pipe(catchError(this.errorHandler));
-    }
-    // AddSignedFileDetail
-    AddSignedFileDetail(signeddetail: LeaseManagement): Observable<any>{
-        return this._httpClient.post<any>(this.baseUrlVsign1 + 'api/Lease/AddSignedFileDetail', signeddetail
-        ) 
-    }
-    //AddSignedFileDetail
-  
-    AddSignedFile(signeddetail: LeaseDocument,selectedFiles: File): Observable<any> {
-        const formData: FormData = new FormData();
-        formData.append(selectedFiles.name, selectedFiles, selectedFiles.name);
-        formData.append('Client', signeddetail.Client);
-        formData.append('Site', signeddetail.Site);
-        formData.append('Company', signeddetail.Company);
-        formData.append('IsDraft', signeddetail.IsDraft.toString());
-        formData.append('DocumentName', signeddetail.DocumentName);
-        formData.append('ContentLength', signeddetail.ContentLength.toString());
-        return this._httpClient.post<any>(this.baseUrlVsign1 + 'api/Lease/AddSignedFile',
-            formData,
-            // {
-            //   headers: new HttpHeaders({
-            //     'Content-Type': 'application/json'
-            //   })
-            // }
-        ).pipe(catchError(this.errorHandler));
+            .pipe(catchError(this.errorHandler));
     }
 }
