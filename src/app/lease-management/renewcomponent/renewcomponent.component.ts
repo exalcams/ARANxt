b704b3c3e189d140a698832dda7aa1988386f1b7
Madhumitha <madhumitha.v@exalca.com>
2021-10-06 09:45:43 +0530

    renew-terminate-vaccate-upload

--- src/app/lease-management/renewcomponent/renewcomponent.component.ts
+++ src/app/lease-management/renewcomponent/renewcomponent.component.ts
@@ -23,11 +23,12 @@ export class RenewcomponentComponent implements OnInit {
   formattedDate: any;
   validMonths: any;
   renewed_Input: Date;
-  renewalById:LeaseRenew[]=[];
-  selectedID:number;
-files:any;
+  renewalById: LeaseRenew[] = [];
+  selectedID: number;
+  files: any;
   filename: any;
-  constructor(public dialogRef: MatDialogRef<RenewcomponentComponent>, private service: LeaseManagementService,private formBuilder: FormBuilder, @Inject(MAT_DIALOG_DATA) public data: any,) {
+  i: number = 0;
+  constructor(public dialogRef: MatDialogRef<RenewcomponentComponent>, private service: LeaseManagementService, private formBuilder: FormBuilder, @Inject(MAT_DIALOG_DATA) public data: any,) {
     this.leaseData = this.data;
     this.leaseID = this.data.leaseID;
     console.log("dialogData", this.leaseData);
@@ -41,14 +42,13 @@ files:any;
   Close(): void {
     this.dialogRef.close();
   }
-  GetLeaseRenewals():void
-  {
-    this.service.GetLeaseRenewals( this.leaseID ).subscribe((data) => {
-this.renewalById=<LeaseRenew[]>data;
-if(this.renewalById.length>0){
-  // this.loadData(this.renewalById[0]);
-}
-console.log("renewalById",this.renewalById);
+  GetLeaseRenewals(): void {
+    this.service.GetLeaseRenewals(this.leaseID).subscribe((data) => {
+      this.renewalById = <LeaseRenew[]>data;
+      if (this.renewalById.length > 0) {
+        // this.loadData(this.renewalById[0]);
+      }
+      console.log("renewalById", this.renewalById);
 
     },
       err => {
@@ -82,97 +82,100 @@ console.log("renewalById",this.renewalById);
     const renew = new LeaseRenew();
     renew.leaseID = this.leaseID;
     renew.renewedOn = this.Vacateformgroup.get('RenewOn').value;
-    renew.expiryDate =this.Vacateformgroup.get('NewExpiryDate').value;
-    renew.validFor=this.Vacateformgroup.get('Validfor').value;
-  renew.revisedRent=this.Vacateformgroup.get('RevisedRent').value;
-  renew.revisedRatio=this.Vacateformgroup.get('Revisedratio').value;
+    renew.expiryDate = this.Vacateformgroup.get('NewExpiryDate').value;
+    renew.validFor = this.Vacateformgroup.get('Validfor').value;
+    renew.revisedRent = this.Vacateformgroup.get('RevisedRent').value;
+    renew.revisedRatio = this.Vacateformgroup.get('Revisedratio').value;
 
-if((this.Vacateformgroup.valid)&&(this.files))
-{
-  this.service.RenewLease(renew, this.files).subscribe((x) => {
-    console.log(x);
-    this.notificationSnackBarComponent.openSnackBar('Uploaded in successfully', SnackBarStatus.success);
-    this.Vacateformgroup.reset();
+    if ((this.Vacateformgroup.valid) && (this.files)) {
+      this.service.RenewLease(renew, this.files).subscribe((x) => {
+        console.log(x);
+        this.notificationSnackBarComponent.openSnackBar('Uploaded in successfully', SnackBarStatus.success);
+        this.Vacateformgroup.reset();
 
-  },
-    err => {
-      console.log(err);
+      },
+        err => {
+          console.log(err);
 
-    })
-}
-else if(!this.files && this.Vacateformgroup.valid)
-{
-  this.notificationSnackBarComponent.openSnackBar('File need to be Uploaded', SnackBarStatus.danger);
-}
-else if(!this.Vacateformgroup.valid)
-{
-  this.Vacateformgroup.setErrors({item:true})
-// this.SignedDocumentDetailsForm.invalid;
-this.ShowValidationErrors(this.Vacateformgroup);
-}
+        })
+    }
+    else if (!this.files && this.Vacateformgroup.valid) {
+      this.notificationSnackBarComponent.openSnackBar('File need to be Uploaded', SnackBarStatus.danger);
+    }
+    else if (!this.Vacateformgroup.valid) {
+      this.Vacateformgroup.setErrors({ item: true })
+      // this.SignedDocumentDetailsForm.invalid;
+      this.ShowValidationErrors(this.Vacateformgroup);
+    }
   }
 
 
   ShowValidationErrors(formGroup: FormGroup): void {
     Object.keys(formGroup.controls).forEach(key => {
-        if (!formGroup.get(key).valid) {
-            console.log(key);
-        }
-        formGroup.get(key).markAsTouched();
-        formGroup.get(key).markAsDirty();
-        if (formGroup.get(key) instanceof FormArray) {
-            const FormArrayControls = formGroup.get(key) as FormArray;
-            Object.keys(FormArrayControls.controls).forEach(key1 => {
-                if (FormArrayControls.get(key1) instanceof FormGroup) {
-                    const FormGroupControls = FormArrayControls.get(key1) as FormGroup;
-                    Object.keys(FormGroupControls.controls).forEach(key2 => {
-                        FormGroupControls.get(key2).markAsTouched();
-                        FormGroupControls.get(key2).markAsDirty();
-                        if (!FormGroupControls.get(key2).valid) {
-                            console.log(key2);
-                        }
-                    });
-                } else {
-                    FormArrayControls.get(key1).markAsTouched();
-                    FormArrayControls.get(key1).markAsDirty();
-                }
+      if (!formGroup.get(key).valid) {
+        console.log(key);
+      }
+      formGroup.get(key).markAsTouched();
+      formGroup.get(key).markAsDirty();
+      if (formGroup.get(key) instanceof FormArray) {
+        const FormArrayControls = formGroup.get(key) as FormArray;
+        Object.keys(FormArrayControls.controls).forEach(key1 => {
+          if (FormArrayControls.get(key1) instanceof FormGroup) {
+            const FormGroupControls = FormArrayControls.get(key1) as FormGroup;
+            Object.keys(FormGroupControls.controls).forEach(key2 => {
+              FormGroupControls.get(key2).markAsTouched();
+              FormGroupControls.get(key2).markAsDirty();
+              if (!FormGroupControls.get(key2).valid) {
+                console.log(key2);
+              }
             });
-        }
+          } else {
+            FormArrayControls.get(key1).markAsTouched();
+            FormArrayControls.get(key1).markAsDirty();
+          }
+        });
+      }
     });
 
-}
+  }
+
   ExpiryCalculation(value: number): void {
+
     let months = value;
     let renew = this.Vacateformgroup.get("RenewOn").value;
-    renew.setMonth(renew.getMonth() + months);
-    let formattedDate = renew.toISOString().slice(0, 10);
+    let newdate = new Date();
+    newdate.setDate(renew.getDate());
+    newdate.setMonth(newdate.getMonth() + months);
+    let formattedDate = newdate.toISOString().slice(0, 10);
     console.log(formattedDate);
     this.Vacateformgroup.get("NewExpiryDate").setValue(formattedDate);
-  }
-  handleFileInput(event):void
-  {
-this.files=event.target.files[0];
-this.filename=event.target.files[0].name;
-console.log("this.files",this.files);
 
   }
-  loadData(row:LeaseRenew):void{
+
+
+  handleFileInput(event): void {
+    this.files = event.target.files[0];
+    this.filename = event.target.files[0].name;
+    console.log("this.files", this.files);
+
+  }
+  loadData(row: LeaseRenew): void {
     console.log("loaddata call");
-    
-    this.selectedID=row.renewalID;
+
+    this.selectedID = row.renewalID;
     this.Vacateformgroup.patchValue({
-      RenewOn:row.renewedOn,
-      Validfor:row.validFor,
-      NewExpiryDate:row.expiryDate,
-      RevisedRent:row.revisedRent,
-      Revisedratio:row.revisedRatio
+      RenewOn: row.renewedOn,
+      Validfor: row.validFor,
+      NewExpiryDate: row.expiryDate,
+      RevisedRent: row.revisedRent,
+      Revisedratio: row.revisedRatio
     });
   }
-  newlease():void
-  {
+  newlease(): void {
     this.Vacateformgroup.reset();
-    this.selectedID=null;
+    this.selectedID = null;
   }
+
   GetRemainingDays(expiry) {
     let today = new Date();
     var diff = Math.floor(new Date(expiry).getTime() - today.getTime());
@@ -183,4 +186,5 @@ console.log("this.files",this.files);
     // var years = Math.floor(months/12);
     return days;
   }
+
 }
